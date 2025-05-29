761020341c3608c98a9b9f8cba141b693209398a
Adam Berki <adee0411@gmail.com>
2025-05-29 08:03:16 +0200

    UI: fixed the hungarian subset problem in font-family

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
 import App from "./App.jsx";
-import "@fontsource/raleway/latin.css";
-import "@fontsource/poppins/latin.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
